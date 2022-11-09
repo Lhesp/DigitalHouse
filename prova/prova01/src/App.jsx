70ff7d8d9c38eeaf89301ce32394db075218f046
Lhesp <luiz.henrique.esp@gmail.com>
2022-11-09 18:13:49 -0300

    Prova Aula 10
    Passo 01

---
+++ prova/prova01/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import Card from './Card';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Form da prova</h1>
+      <form action="">
+
+
+
+      </form>
+
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ prova/prova01/src/Card.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Card = () => {
+    return <div>Olá, eu sou um card.</div>
+};
+
+
+export default Card;
